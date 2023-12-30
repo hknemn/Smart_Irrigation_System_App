871507eacbcf88bd843883cbf44dd2a1b474ea5e
Hakan Emin Ünal <20010310044@ogrenci.bartin.edu.tr>
2023-12-30 20:52:59 +0300

    update

--- Pages/LiveData.xaml.cs
+++ Pages/LiveData.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Wpf;
 
 namespace Smart_Irrigation_System.Pages
 {
